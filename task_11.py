5c6c95b3ae236b698acd37778bf22db70bcf8b27
sasha-pipec <97446158+sasha-pipec@users.noreply.github.com>
2022-01-11 20:07:10 +0300

    Update task_11

--- task_11.py
+++ task_11.py
@@ -1,33 +1,22 @@
 class Dessert:
 
-    def __init__(self, name='', calories=0):
+    def __init__(self, name='', calories=''):
         self.name = name
         self.calories = calories
-
-    def setName(self, name):
-        self.name = name
-
-    def getName(self):
-        return self.name
-
-    def setCalories(self, calories):
-        self.calories = calories
-
-    def getCalories(self):
-        return self.calories
 
     def is_healthy(self):
-        if type(self.calories) == int or float:
+        if type(self.calories) == str:
+            return False
+        else:
             if self.calories < 200:
                 return True
             else:
                 return False
-        else:
-            return False
 
     def is_delicious(self):
         return True
 
 
-pt = Dessert('cake', 201)
-print(pt.is_healthy(), pt.is_delicious())
+pt = Dessert()
+pt.calories = 200
+print(pt.is_healthy())
